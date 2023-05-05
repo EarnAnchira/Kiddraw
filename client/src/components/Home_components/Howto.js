d7c792dbbb24e978943c8882bba4dbf9ba1a6866
CHATCHANOK VITHOONDEJ <chatchanok.v@mail.kmutt.ac.th>
2023-05-06 05:06:10 +0700

    [Refactor] Home UxUi

---
+++ client/src/components/Home_components/Howto.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Howto = () => {
+    return (
+        <>
+            <section class="main-section HowTo-section">
+                <div class="container">
+
+                    <h1 className='HowTo'>HOW TO PLAY</h1>
+
+                    <div class="Howto-details">
+                        <div class="HowTo-video">
+                
+                        </div>
+                    
+                        <div class="HowTo-description">
+                            <div class="content">
+                                <img width="200" src="/assest/navbarImg/logo_white.png"></img>
+                                <h4 class="content-header">KID-DRAW-CLUB</h4>
+                                <p class="content-text">
+                                    The detail about this kid-draw-club. who are we and what can we do. et cetera.
+                                    The detail about this kid-draw-club. who are we and what can we do. et cetera.
+                                    The detail about this kid-draw-club. who are we and what can we do. et cetera.
+                                </p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+export default Howto
