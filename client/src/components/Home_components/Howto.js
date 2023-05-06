ac9706ba3a617df5e297027c7533ee8feaa432ef
CHATCHANOK VITHOONDEJ <chatchanok.v@mail.kmutt.ac.th>
2023-05-06 14:49:01 +0700

    [Refactor] video iframe

--- client/src/components/Home_components/Howto.js
+++ client/src/components/Home_components/Howto.js
@@ -11,27 +11,9 @@ const Howto = () => {
 
                     <div class="Howto-details">
                         <div class="HowTo-video">
-                            <div
-        className="video"
-        style={{
-          position: "relative",
-          paddingBottom: "56.25%" /* 16:9 */,
-          paddingTop: 25,
-          height: 0
-        }}
-      >
-        <iframe
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%"
-          }}
-          src={`https://www.youtube.com/embed/wBgTn4GWDtQ`}
-        />
-      </div>
-                            
+                            <div className="video" style={{position: "relative", paddingBottom: "56.25%", paddingTop: 25, height: 0}}>
+                                <iframe style={{position: "absolute",top: 0, left: 0, width: "100%", height: "100%"}} src={`https://www.youtube.com/embed/wBgTn4GWDtQ`}/>
+                            </div>                   
                         </div>
                     
                         <div class="HowTo-description">
